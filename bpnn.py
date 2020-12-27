d7010ea685cb1b46968332c22bb6e044ae32fad6
piao_xue <piaoxuegd@gmail.com>
2020-12-27 17:56:23 +0800

    finish the training part

---
+++ bpnn.py
@@ -0,0 +1,75 @@
+import numpy as np
+import random
+
+trainDataPath = "D:\\lgs\\MySoftware\\python\\bpNN\\trainingdata\\train-images.idx3-ubyte"
+trainDataOffset = 16
+trainLablePath = "D:\\lgs\\MySoftware\\python\\bpNN\\trainingdata\\train-labels.idx1-ubyte"
+trainLableOffset = 8
+testDataPath = "D:\\lgs\\MySoftware\\python\\bpNN\\trainingdata\\t10k-images.idx3-ubyte"
+testDataOffset = 16
+testLablePath = "D:\\lgs\\MySoftware\\python\\bpNN\\trainingdata\\t10k-labels.idx1-ubyte"
+testLableOffset = 8
+
+step = 0.1  #学习率
+
+#读取数据
+imageFile = open(trainDataPath, "rb")
+imageFile.seek(trainDataOffset)
+lableFile = open(trainLablePath, "rb")
+lableFile.seek(trainLableOffset)
+Datas = [[np.frombuffer(imageFile.read(784), np.uint8).reshape((1, 784)) / 255, lableFile.read(1)[0]] for x in range(60000)]
+
+testImageFile = open(testDataPath, "rb")
+testImageFile.seek(testDataOffset)
+testLableFile = open(testLablePath, "rb")
+testLableFile.seek(testLableOffset)
+testData = [[np.frombuffer(testImageFile.read(784), np.uint8).reshape((1, 784)) / 255, testLableFile.read(1)[0]] for x in range(10000)]
+
+k1, k2 = np.ones((785, 32)), np.ones((33, 10))  #初始化参数列表
+
+def sigmoid(m):
+    return 1 / (1 + np.exp(-m))
+
+def dSigmoid(m):    #也等于sigmoid(m)*(1-sigmoid(m))
+    expNegm = np.exp(-m)
+    return expNegm / np.square((1 + expNegm))
+
+def wantedArray(n):
+    return np.array([1 if x == n else 0 for x in range(10)]).reshape((1, 10))
+
+def training(dataList):
+    global k1
+    global k2
+    dk1, dk2 = np.zeros((785, 32)), np.zeros((33, 10))
+
+    for data in dataList:
+        L1 = np.append(data[0], [1]).reshape((1, 785))  #加入偏置
+        L2 = np.append(sigmoid(L1 @ k1), [1]).reshape((1, 33))  #加入偏置
+        L3 = sigmoid(L2 @ k2)
+        w = wantedArray(data[1])
+        delta3 = (w - L3) * dSigmoid(L3)
+        dk2 += L2.T @ delta3
+        delta2 = (k2 @ delta3.reshape(10)) * dSigmoid(L2)
+        dk1 += L1.T @ (delta2.reshape(33)[:-1].reshape((1, 32)))    #把delta2削掉最后一个数来适配大小
+    
+    dk1 /= len(dataList)
+    dk2 /= len(dataList)
+    k1 -= step * dk1
+    k2 -= step * dk2
+
+
+def loss(out, n):
+    return 0.5 * np.sum(np.square(out - wantedArray(n)))
+
+'''
+def getAns(m):
+    L1 = np.append(m, [1]).reshape((1, 785))  #加入偏置
+    L2 = np.append(sigmoid(L1 @ k1), [1]).reshape((1, 33))  #加入偏置
+    L3 = sigmoid(L2 @ k2)
+'''
+
+for i in range(1):
+    random.shuffle(Datas)
+    for j in range(600):
+        training(Datas[j * 100 : (j + 1) * 100])
+    
--- testnumpy.py
+++ testnumpy.py
@@ -1,12 +1,22 @@
 import numpy as np
+from PIL import Image
 
 '''
 matrix1 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 print(matrix1 @ matrix1)
 '''
 
-f = open("D:\\lgs\\MySoftware\\python\\bpNN\\trainingdata\\t10k-images.idx3-ubyte", "rb")
-f.seek(16)
+trainDataPath = "D:\\lgs\\MySoftware\\python\\bpNN\\trainingdata\\train-images.idx3-ubyte"
+trainDataOffset = 16
+trainLablePath = "D:\\lgs\\MySoftware\\python\\bpNN\\trainingdata\\train-labels.idx1-ubyte"
+trainLableOffset = 8
 
-#vector1 = np.array(f.read(28*28), np.uint8)
-#print(vector1)
+imageFile = open(trainDataPath, "rb")
+imageFile.seek(trainDataOffset)
+lableFile = open(trainLablePath, "rb")
+lableFile.seek(trainLableOffset)
+
+#Datas = [[np.frombuffer(imageFile.read(784), np.uint8).reshape((1, 784)) / 255, lableFile.read(1)[0]] for x in range(100)]
+Datas = [[np.frombuffer(imageFile.read(784), np.uint8).reshape((1, 784)), lableFile.read(1)[0]] for x in range(100)]
+im = Image.fromarray(Datas[0][0].reshape((28, 28)), "L")
+im.save("D:\\lgs\\MySoftware\\python\\bpNN\\image\\test.png", "PNG")
